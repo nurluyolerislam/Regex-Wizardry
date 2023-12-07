9e84421c2f17f0ee1d7d6f8fb090e2409532e6fd
Erislam Nurluyol <nurluyolerislam@gmail.com>
2023-12-07 22:08:14 +0300

    Initial Commit

---
+++ Regex Wizardry/Regex_WizardryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Regex_WizardryApp.swift
+//  Regex Wizardry
+//
+//  Created by Erislam Nurluyol on 7.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct Regex_WizardryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
